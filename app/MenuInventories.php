2b220f22a4c7f1d0b3252c8e5ae5d944e269dc91
hanmajid <han.majid2004@gmail.com>
2017-04-01 12:35:34 +0700

    init models

---
+++ app/MenuInventories.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MenuInventories extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'menus_inventories';
+}
