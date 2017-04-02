a4a4ae6eb4d393ac52d9b18d9a84cc2e3c7a3312
hanmajid <han.majid2004@gmail.com>
2017-04-02 13:45:08 +0700

    add add menu feature

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -58,7 +58,7 @@
                     <div class="panel-body">
                         <!-- Form Select Menu 1 -->
                         <div class="row">
-                            <div class="form-group{{ $errors->has('menu_name') ? ' has-error' : '' }}">
+                            <div class="form-group">
                                 <label for="menu_name" class="col-md-3 control-label">Search Menu</label>
                                 <div class="col-md-9">
                                     <input id="menu_name" type="text" class="form-control" name="menu_name" onkeydown="validate(event)" autocomplete="off">
@@ -85,12 +85,59 @@
                     </div>
                 </div>
 
-                <div id="form-select-menu-2"  style="display: none;">
-                    <div class="panel-heading"><span id="menuSelected"></span></div>
+                <div id="form-select-menu-2"  style="display: none; max-height:500px; min-height:500px; overflow-y: scroll;">
+                    <div class="panel-body row">
+                        <div class="col-md-12">
+                            <p id="Back" onclick="selectMenu(id)" style="cursor: pointer;">Back</p>
+                        </div>
 
-                    <div class="panel-body">
-                        <span id="Back" onclick="selectMenu(id)" style="cursor: pointer;">Back</span>
-                        
+                        <div class="col-md-4">
+                            <img id="imgMenuSelected" class="img-thumbnail">
+                        </div>      
+
+                        <div class="col-md-8">
+                            <h3 id="menuSelected"></h3>
+                            <p>Price: Rp10.000,00</p>
+                            <p>Stock: 10 buah</p>
+                        </div>                  
+
+                        <div class="col-md-12">
+                            <label for="quantity" class="col-md-12 control-label">Quantity</label>
+                            <div class="col-md-12">
+                                <input id="quantity" type="text" class="form-control" name="quantity" autocomplete="off">
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <label class="col-md-12 control-label">Discount</label>
+                            <div class="col-md-5 col-md-offset-1">
+                                <div class="checkbox">
+                                    <input type="checkbox"> Discount 1
+                                </div>
+                            </div>
+                            <div class="col-md-5 col-md-offset-1">
+                                <div class="checkbox">
+                                    <input type="checkbox"> Discount 1
+                                </div>
+                            </div>
+                            <div class="col-md-5 col-md-offset-1">
+                                <div class="checkbox">
+                                    <input type="checkbox"> Discount 1
+                                </div>
+                            </div>                        
+                        </div>
+
+                        <div class="col-md-12">
+                            <label for="description" class="col-md-12 control-label">Description</label>
+                            <div class="col-md-12">
+                                <textarea id="description" name="description" rows="5" cols="50"></textarea>
+                            </div>  
+                        </div>
+
+                        <div class="col-md-offset-9 col-md-3">
+                            <br />
+                            <button class="btn btn-default">Add to Order</button>
+                        </div>
                     </div> <!-- End of Form Select Menu 2 -->
                 </div>          
             </div>
@@ -104,8 +151,11 @@
         $("#form-select-menu-2").toggle();
         if(menuId != "Back") {
             alt = $("#"+menuId).attr("alt")
+            src = $("#"+menuId).attr("src")
             console.log(alt)
+            console.log(src)
             $("#menuSelected").text(alt)
+            $("#imgMenuSelected").attr("src",src)
         }
     }
 
@@ -126,7 +176,6 @@
                 }
             });
         }
-        //validation of the input...
     }
 </script>
 @endsection
