efcc9a87515511944b76fc369cb9d8a20272207f
hanmajid <han.majid2004@gmail.com>
2017-04-11 01:16:30 +0700

    update pos index again

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -34,13 +34,26 @@
                         </div>
 
                         <h4>Purchases</h4>
-                            <div id="purchases">
-                                <span>Empty</span>
-                            </div>
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Menu</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Total</th>
+                                        <th>Description</th>
+                                        <th></th> <!-- Edit Button -->
+                                        <th></th> <!-- Delete Button -->
+                                    </tr>
+                                </thead>
+                                <tbody id="purchases">
+                                </tbody>
+                            </table>
 
                         <div class="form-group" style="position: absolute; bottom: 20px;">
                             <div class="col-md-6">
-                                <button type="submit" class="btn btn-primary">
+                                <button class="btn btn-primary" id="save_order">
                                     Save Order
                                 </button>
                             </div>
@@ -96,7 +109,8 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <span onclick="selectMenu(id)" style="cursor: pointer;"><i class="fa fa-chevron-left" aria-hidden="true"></i> Back</span>
-                                <button class="btn btn-default pull-right" onclick="addToOrder()">Add to Order</button>
+                                <button id="add_to_order" class="btn btn-default pull-right" onclick="addToOrder()">Add to Order</button>
+                                <button id="save_changes" class="btn btn-success pull-right" onclick="saveChanges()">Save Changes</button>
                                 <span class="pull-right menuTitle" id="menuSelected" style="margin-right: 10px; margin-top: 5px;"></span>
                                 <hr />
                             </div>
@@ -129,7 +143,7 @@
                                     @foreach ($promotions as $promotion)
                                         <div class="col-md-5 col-md-offset-1">
                                             <div>
-                                                <input class="promotion-checkbox" type="checkbox" id="promotion{{ $promotion->id }}" discount="{{ $promotion->discount }}" onclick="onClickPromotion()" /> {{ $promotion->name }} ({{ $promotion->discount }}%)
+                                                <input class="promotion-checkbox" type="checkbox" id="promotion{{ $promotion->id }}" discount="{{ $promotion->discount }}" onclick="updateTotalPrice()" /> {{ $promotion->name }} ({{ $promotion->discount }}%)
                                             </div>
                                         </div>
                                     @endforeach
@@ -151,7 +165,9 @@
 
                         <div class="row">
                             <div class="col-md-12">
-                                Total: Rp<span class="menuTitle" id="totalPrice"></span>,00
+                                <span class="menuTitle">
+                                    Total: Rp<span id="totalPrice"></span>,00
+                                </span>
                             </div>                                
                         </div>
                     </div> <!-- End of Form Select Menu 2 -->
@@ -164,44 +180,251 @@
 
 @section('scripts')
 <script type="text/javascript">
-    function selectMenu(menuId) {
+    // 'Getters'
+    function getTotalDiscount() {
+        total_discount = 0
+        $(".promotion-checkbox").each(function(index) {
+            var id = $( this ).attr('id')
+            // console.log(id)
+            if(document.getElementById(id).checked) {
+                var discount = $( this ).attr('discount')
+                // console.log('id: '+id+ ' | discount: '+discount)
+                total_discount += discount
+            }
+        });
+        return total_discount
+    }
+
+    function getQuantity() {
+        return $("#quantity").val()
+    }
+
+    function getTotalPrice() {
+        return $("#totalPrice").text()
+    }
+
+    function getDescription() {
+        return $("#description").val()   
+    }
+
+    function getMenuName() {
+        return $("#menuSelected").text()
+    }
+
+    function getOriginalPrice() {
+        return $("#priceMenuSelected").text()   
+    }
+
+    function countNewItems() {
+        count = 0
+        $(".new-item").each(function(index) {
+            count++
+        });
+        return count
+    }
+
+    // 'Setters'
+    function setMenuName(menu_name) {
+        $("#menuSelected").text(menu_name)
+    }
+
+    function setMenuImgSrc(src) {
+        $("#imgMenuSelected").attr("src",src)
+    }
+
+    function setOriginalPrice(price) {
+        $("#priceMenuSelected").text(price)
+    }
+
+    function setTotalPrice(total_price) {
+        $("#totalPrice").text(total_price)
+    }
+
+    function toggleForm() {
         $("#form-select-menu-1").toggle();
         $("#form-select-menu-2").toggle();
-        if(menuId != "Back") {
+    }
+
+
+    function selectMenu(menuId) {
+        toggleForm()
+        // console.log(menuId)
+
+        if(menuId != "") {
+            $('#save_changes').hide()
+            $('#add_to_order').show()
             alt = $("#menu"+menuId).attr("alt")
             src = $("#menu"+menuId).attr("src")
             price = $("#menu"+menuId).attr("price")
             // console.log(alt)
             // console.log(src)
-            $("#menuSelected").text(alt)
-            $("#imgMenuSelected").attr("src",src)
-            $("#priceMenuSelected").text(price)
-            $("#totalPrice").text(price)
+            setMenuName(alt)
+            setMenuImgSrc(src)
+            setOriginalPrice(price)
+            setTotalPrice(price)
+            // Uncheck all promotions checkboxs
+            $(".promotion-checkbox").each(function(index) {
+                $(this).prop('checked', false)
+            });
+            // Reset quantity to 1
+            $("#quantity").val(1)
+        }
+        else {
+            // Back button is pressed
+            resetColorTableRow()
+            showAllButtons()
+            $('#save_order').removeClass('disabled')
+            $('#customer_name').removeClass('disabled')
+            var selectize = $select[0].selectize;
+            selectize.enable()
         }
     }
 
-    function onClickPromotion() {
-        $(".promotion-checkbox").each(function(index) {
-            var id = $( this ).attr('id')
-            // console.log(id)
-            total_discount = 0
-            if(document.getElementById(id).checked) {
-                var discount = $( this ).attr('discount')
-                console.log('id: '+id+ ' | discount: '+discount)
-                total_discount += discount
-            }
-            calculateTotalPrice(total_discount)
-        });
+    function updateTotalPrice() {
+        total_discount = getTotalDiscount()
+        quantity = getQuantity()
+        calculateTotalPrice(total_discount, quantity)
     }
 
-    function calculateTotalPrice(total_discount) {
-        original_price = $("#priceMenuSelected").text()
-        discounted_price = original_price - (original_price*total_discount/100)
+    function calculateTotalPrice(total_discount, quantity) {
+        original_price = getOriginalPrice()
+        price_quantity = original_price * quantity
+        discounted_price = price_quantity - (price_quantity*total_discount/100)
         $("#totalPrice").text(discounted_price)
     }
 
     function addToOrder() {
+        menu_name = getMenuName()
+        original_price = getOriginalPrice()
+        quantity = getQuantity()
+        total_price = getTotalPrice()
+        description = getDescription()
+        // console.log(quantity)
+        // console.log(total_price)
+        // console.log(description)
+        addToList(menu_name, original_price, quantity, total_price, description)
+        toggleForm()
+    }
 
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function addToList(menu_name, original_price, quantity, total_price, description) {
+        no = countNewItems() + 1
+        id = getRandomInt(0,1000000)
+        delete_button = '<span onclick="removeFromList(id)" id="'+id+'" style="cursor: pointer;"><i class="fa fa-trash" aria-hidden="true"></i></span>'
+        edit_button = '<span onclick="editMenu(id)" id="'+id+'" style="cursor: pointer;"><i class="fa fa-pencil" aria-hidden="true"></i></span>'
+
+        td_no = '<td>'+no+'</td>'
+        td_menu = '<td>'+menu_name+'</td>'
+        td_price = '<td>'+original_price+'</td>'
+        td_quantity = '<td>'+quantity+'</td>'
+        td_total = '<td>'+total_price+'</td>'
+        td_description = '<td>'+description+'</td>'
+        td_edit_button = '<td>'+edit_button+'</td>'
+        td_delete_button = '<td>'+delete_button+'</td>'
+
+        new_item = '<tr class="new-item">'
+            +td_no
+            +td_menu
+            +td_price
+            +td_quantity
+            +td_total
+            +td_description
+            +td_edit_button
+            +td_delete_button
+            +'</tr>'
+
+        $('#purchases').append(new_item)
+    }
+
+    function removeFromList(id) {
+        // console.log(id)
+        row = $('#'+id).parent().parent()
+        row.remove()
+        rewriteTableNo()
+    }
+
+    function rewriteTableNo() {
+        count = 1;
+        $(".new-item").each(function(index) {
+            $(this).children(':first').text(count)
+            count++
+        });
+    }
+
+    function colorBlueTableRow(id) {
+        row = $('#'+id).parent().parent()
+        row.addClass('info')
+    }
+
+    function resetColorTableRow() {
+        $(".new-item").each(function(index) {
+            $(this).removeClass('info')
+        });
+    }
+
+    function hideAllButtons() {
+        $(".new-item").each(function(index) {
+            edit_button = $(this).children().eq(6).children().eq(0)
+            // console.log(edit_button)
+            edit_button.hide()
+            delete_button = $(this).children().eq(7).children().eq(0)
+            // console.log(delete_button)
+            delete_button.hide()
+        });
+    }
+    function showAllButtons() {
+        $(".new-item").each(function(index) {
+            edit_button = $(this).children().eq(6).children().eq(0)
+            // console.log(edit_button)
+            edit_button.show()
+            delete_button = $(this).children().eq(7).children().eq(0)
+            // console.log(delete_button)
+            delete_button.show()
+        });
+    }
+
+    function editMenu(id) {
+        // console.log("editMenu: "+id)
+        colorBlueTableRow(id)
+        toggleForm()
+
+        // Disable all other buttons
+        hideAllButtons()
+        $('#save_order').addClass('disabled')
+        $('#save_order').addClass('disabled')
+        var selectize = $select[0].selectize;
+        selectize.disable()
+        $('#save_changes').show()
+        $('#add_to_order').hide()
+
+        row = $('#'+id).parent().parent()        
+        row.children().each(function(index) {
+            if(index == 1) { // menu name
+                menu_name = $(this).text()
+            }
+            else if(index == 2) { // original price
+                original_price = $(this).text()
+            }
+            else if(index == 3) { // quantity
+                quantity = $(this).text()
+            }
+            else if(index == 4) { // total_price
+                total_price = $(this).text()
+            }
+              
+        })
+        setMenuName(menu_name)
+        // setMenuImgSrc(src)
+        setOriginalPrice(original_price)
+        setTotalPrice(total_price)
+        // // Uncheck all promotions checkboxs
+        // $(".promotion-checkbox").each(function(index) {
+        //     $(this).prop('checked', false)
+        // });
+        $("#quantity").val(quantity)
     }
 
 
@@ -235,9 +458,7 @@
         // console.log(value)
         if(isInt(value)) {
             quantity.val(parseInt(value)+1)
-        }
-        else {
-
+            updateTotalPrice()
         }
     }
 
@@ -248,17 +469,15 @@
         if(isInt(value)) {
             if(parseInt(value) > 1) {
                 quantity.val(parseInt(value)-1)
+                updateTotalPrice()
             }
-        }
-        else {
-
         }
     }
 </script>
 
 <script src="{{ asset('js/selectize.js') }}"></script>
 <script>
-    $('#customer_name').selectize({
+    var $select = $('#customer_name').selectize({
         create: true,
         sortField: 'text'
     });
